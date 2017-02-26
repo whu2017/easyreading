82339a84bae5d7122a8cc5aa1b1d8560b18ead68
doraemonext <doraemonext@gmail.com>
2017-02-26 21:04:44 +0800

    调整部署脚本位置

---
+++ .travis/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ $TRAVIS_BRANCH == "master" ]; then
+    sshpass -p "${MASTER_PASS}" ssh -o "StrictHostKeyChecking no" master@oott.me "/bin/sh ~/server/build.sh"
+elif [ $TRAVIS_BRANCH == "develop" ]; then
+    sshpass -p "${DEVELOP_PASS}" ssh -o "StrictHostKeyChecking no" develop@oott.me "/bin/sh ~/server/build.sh"
+elif [ $TRAVIS_BRANCH == "xugengtao" ]; then
+    sshpass -p "${XUGENGTAO_PASS}" ssh -o "StrictHostKeyChecking no" xugengtao@oott.me "/bin/sh ~/server/build.sh"
+elif [ $TRAVIS_BRANCH == "xuyichao" ]; then
+    sshpass -p "${XUYICHAO_PASS}" ssh -o "StrictHostKeyChecking no" xuyichao@oott.me "/bin/sh ~/server/build.sh"
+elif [ $TRAVIS_BRANCH == "liguangjun" ]; then
+    sshpass -p "${LIGUANGJUN_PASS}" ssh -o "StrictHostKeyChecking no" liguangjun@oott.me "/bin/sh ~/server/build.sh"
+elif [ $TRAVIS_BRANCH == "guoyaoxing" ]; then
+    sshpass -p "${GUOYAOXING_PASS}" ssh -o "StrictHostKeyChecking no" guoyaoxing@oott.me "/bin/sh ~/server/build.sh"
