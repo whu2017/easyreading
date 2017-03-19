4019ecdf412cb28be66cb243969751e6931d29ff
doraemonext <doraemonext@gmail.com>
2017-03-19 21:36:58 +0800

    添加 django admin jet

--- easyreading/settings.py
+++ easyreading/settings.py
@@ -34,6 +34,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jet.dashboard',
+    'jet',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
