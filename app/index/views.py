390cf4079473a3c311ef198909c894951e5c3db3
doraemonext <doraemonext@gmail.com>
2017-03-25 11:32:53 +0800

    更新主页

--- app/index/views.py
+++ app/index/views.py
@@ -3,14 +3,7 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-from django.contrib.auth.models import User
 
 
 def index_view(request):
-    users = User.objects.filter(is_superuser=True).order_by("id")
-    branch = ""
-    if len(users) > 0:
-        branch = users[0].username
-    return render(request, "index.html", {
-        "branch": branch,
-    })
+    return render(request, "index.html", {})
