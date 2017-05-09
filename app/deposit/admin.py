b2e0fb8aa3a61006daa138bfa8ddb28880d86425
doraemonext <doraemonext@gmail.com>
2017-05-09 10:13:44 +0800

    fix

--- app/deposit/admin.py
+++ app/deposit/admin.py
@@ -8,5 +8,8 @@ from app.deposit.models import Deposit
 class DepositAdmin(admin.ModelAdmin):
     list_display = ('user', 'amount', 'status', 'create_timestamp', 'modify_timestamp')
 
+    def has_add_permission(self, request):
+        return False
+
 
 admin.site.register(Deposit, DepositAdmin)
--- check/admin.py
+++ check/admin.py
@@ -8,6 +8,8 @@ from check.models import Check
 class CheckAdmin(admin.ModelAdmin):
     list_display = ('user', 'check_timestamp')
 
+    def has_add_permission(self, request):
+        return False
+
 
 admin.site.register(Check, CheckAdmin)
-
